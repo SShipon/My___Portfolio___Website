096566bc4d6e50f910013ea9fd37aba859d7ea69
SShipon <nxshipon@gmail.com>
2023-12-28 00:58:34 -0800

    hea

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,9 +2,6 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar/Navbar";
 import Footer from "../shared/Footer/Footer";
 
-
-
-
 const Main = () => {
     return (
         <div>
